0bae8b58dacd568c4f863ffed02599945d4d55c1
Britney Lyons <blyons333@gmail.com>
2015-03-14 23:20:10 -0500

    added menu toggle

--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,11 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('#menu-toggle').click(function(){
+  	if ($('.menu-item').is(":visible")) {
+  		$('.menu-item').hide();
+  	}else{
+  		$('.menu-item').show();
+  	}
+  });
 });
